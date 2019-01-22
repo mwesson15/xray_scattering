5072ebb850c418a549b597a76fd8929e6bcfbe1a
dyunis <dyunis@uchicago.edu>
2019-01-22 14:08:27 -0600

    better geom approx

--- soltwisch2017.py
+++ soltwisch2017.py
@@ -4,32 +4,60 @@ import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+'''
+TODO: make sure resolutions, lengthscales, source frequencies make sense
+https://meep.readthedocs.io/en/latest/Introduction/#units-in-meep
+'''
+
+'''
+TODO: make reflectance plots (Fig 5, Fig 6)
+https://meep.readthedocs.io/en/latest/Python_Tutorials/Basics/#angular-reflectance-spectrum-of-a-planar-interface
+https://meep.readthedocs.io/en/latest/Python_Tutorials/Mode_Decomposition/#reflectance-and-transmittance-spectra-for-planewave-at-oblique-incidence
+'''
+
 #soltwisch use finite element to do the simulation, not FDTD
 #based on examples/binary_grating.py
 
 #set up geometry
 resolution = 10        # pixels/μm
 
-dpml = 1.0             # PML thickness
-dsub = 3.0             # substrate thickness
-dpad = 3.0             # padding between grating and PML
-gp = 10.0              # grating period
-gh = 5.0               # grating height
-gdc = 0.5              # grating duty cycle
+#G2 params in 10^-8m
+dpml = 1.             # PML thickness
+dsub = 3.             # substrate thickness
+dpad = 3.             # air padding between grating and PML at top
+gp = 15.             # grating period
+gh = 11.950               # grating height
+lw = 6.730              #line width
+tr = 0.916              #top radius
+br = 1.302             #bottom radius
+sa = 84.73              #sidewall angle (deg)
+
+a = gh/np.tan(sa*2*np.pi/360) # intermediate for vertex calculation
 
 sx = dpml+gp+dpml
 sy = dpml+dsub+gh+dpad+dpml
 
 cell = mp.Vector3(sx,sy,0)
-pml_layers = [mp.PML(thickness=dpml,direction=mp.Y)]
+pml_layers = [mp.PML(thickness=dpml)]
+#pml_layers = [mp.PML(thickness=dpml,direction=mp.Y)]
 
-#make sure materials are right
+#check we have right epsilon
 substrate = mp.Medium(epsilon=12)
 grating = mp.Medium(epsilon=12)
-#one for substrate, one for grating superstructure, using rectangles to approximate right now
+
+#one for substrate, one for grating superstructure, using trapezoid prism to approximate right now
+vertices = [mp.Vector3(-0.5*lw-0.5*a,-0.5*gh,0),
+            mp.Vector3(0.5*lw+0.5*a,-0.5*gh,0),
+            mp.Vector3(0.5*lw-0.5*a,0.5*gh,0),
+            mp.Vector3(-0.5*lw+0.5*a,0.5*gh,0)]
+
+#mp.Block(material=grating, size=mp.Vector3(lw,gh,mp.inf), center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0)),
+
 geometry = [mp.Block(material=substrate, size=mp.Vector3(sx,dpml+dsub,mp.inf), center=mp.Vector3(0,-0.5*sy+0.5*(dpml+dsub),0)),
-            mp.Block(material=grating, size=mp.Vector3(gdc*gp,gh,mp.inf), center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0))]
-symmetries=[mp.Mirror(mp.X)]
+            #mp.Block(material=grating, size=mp.Vector3(lw,gh,mp.inf), center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0))]
+            mp.Prism(vertices, height=mp.inf, center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0), material=grating)]
+# symmetries=[mp.Mirror(mp.X)]
+symmetries = []
 
 #set up source
 wvl_min = 0.4           # min wavelength
@@ -39,13 +67,13 @@ fmax = 1/wvl_min        # max frequency
 fcen = 0.5*(fmin+fmax)  # center frequency
 df = fmax-fmin          # frequency width
 
-theta = 0.3 * 2*np.pi/360 #in fig 5a these are in interval [0.3,1.2]
+theta = 0.86 * 2*np.pi/360 #in fig 5a these are in interval [0.3,1.2]
 k = mp.Vector3(0,-np.sin(theta),np.cos(theta)) # complex fields for oblique angles
 src_pt = mp.Vector3(0,-0.5*sy+dpml+dsub+gh,0) # incident at top of grating
 
 #in meep, frequency units are in units of 2*pi*c
 #read more carefully how they set up simulation of source in paper, this is just a plane hitting the top of grating
-sources = [mp.Source(mp.ContinuousSource(frequency=0.15), component=mp.Ez, center=src_pt, size=mp.Vector3(0.5*sx,0,0))]
+sources = [mp.Source(mp.ContinuousSource(frequency=100), component=mp.Ez, center=src_pt, size=mp.Vector3(sx,0,0))]
 
 sim = mp.Simulation(resolution=resolution,
                     cell_size=cell,
@@ -54,11 +82,10 @@ sim = mp.Simulation(resolution=resolution,
                     k_point=k,
                     sources=sources,
                     symmetries=symmetries)
-sim.run(until=200)
+sim.run(until=20)
 
 eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
 ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
-print(np.unique(eps_data))
 
 #plot geoms
 plt.figure(dpi=100)
