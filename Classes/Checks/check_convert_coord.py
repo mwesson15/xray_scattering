8dc8876fd517161d108064c2d96d345befd54694
Jayce1234 <43833340+Jayce1234@users.noreply.github.com>
2019-01-29 20:45:41 -0600

    Add files via upload

---
+++ Classes/Checks/check_convert_coord.py
@@ -0,0 +1,21 @@
+import numpy as np
+from general import general_functions as gf
+
+x = 0
+y_min = -10
+y_max = 10
+
+origin = [-10, 3]
+
+delta_y = 1
+y = np.arange(y_min, y_max, delta_y)
+
+coords = gf.convert_coord(x, y_min, y_max, origin, delta_y)
+
+print(coords)
+
+xy_mag = (x-origin[0])**2 + np.square(y-origin[1])
+rz_mag = np.square(coords[0,:]) + np.square(coords[1,:])
+
+#print(xy_mag - rz_mag)
+#The difference between the two magintudes
