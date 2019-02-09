a4d93605b668b8c87fd4961f7ed5027f4d4c5cec
mwesson15 <mwesson@uchicago.edu>
2019-02-09 12:21:25 -0600

    Add files via upload

--- Marie_edits/sphere_scattering.py
+++ Marie_edits/sphere_scattering.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import h5py
+import cmath
+import math
+from materials import Gold, Silicon, Materials
 
 class globalVariables():
     cellXSize = 100
@@ -12,13 +15,22 @@ class globalVariables():
     # indexOfSphere = 3.4
 
     sphereRadius = 20
-    resolution = 100
+    resolution = 10
     projectName = "sphere_scattering"
     h5Name = "ez"
     imageStorageDirectory = projectName +'-out'
 
 def generateSphere(radius):
-    sphere = mp.Cylinder(radius=radius, material=mp.Medium(index=globalVariables.indexOfSphere))
+    si = Gold()
+    ind = si.index_refract()
+    ind = ind.real
+    cond = ind.imag
+    wv = 0.0167
+    freq = 1/wv
+    mat = mp.Medium(index=ind, D_conductivity=2*math.pi*freq*cond/ind)
+    # sphere = mp.Cylinder(radius=radius, material=mp.Medium(index=globalVariables.indexOfSphere))
+    sphere = mp.Cylinder(radius=radius, material=mat)
+
     return sphere
 
 def createSimulationSpace():
@@ -33,17 +45,44 @@ def createSimulationSpace():
 
     return cell,geometry,sources,pml_layers
 
+def pw_amp(k,x0):
+    def _pw_amp(x):
+        return cmath.exp(1j*2*math.pi*k.dot(x+x0))
+    return _pw_amp
+
 def generateSource():
-    sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+
+    use_cw_solver = False
+    src_pt = mp.Vector3(-49,0,0)
+    ng = 1
+    fcen = 0.15
+    wv = 0.0167
+    fcen = 1/wv
+    df = 0.1
+    theta_in = 0
+    sy = 100
+    k = mp.Vector3(math.cos(theta_in),math.sin(theta_in),0).scale(fcen*ng)
+    sources = [mp.Source(mp.ContinuousSource(fcen,fwidth=df) if use_cw_solver else mp.GaussianSource(fcen,fwidth=df),
                      component=mp.Ez,
-                     center=mp.Vector3(0, 0),
-                     size=mp.Vector3(50, 10, 0),
-                     amp_func=amp),
-           mp.Source(mp.CustomSource(src_func=time_dependence),
-                     component=mp.Ez,
-                     center=mp.Vector3(0, 0),
-                     size=mp.Vector3(50, 10, 0),
-                     amp_func=amp)]
+                     center=src_pt,
+                     size=mp.Vector3(0,sy,0),
+                     amp_func=pw_amp(k,src_pt))]
+
+    # sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+    #                  component=mp.Ez,
+    #                  center=mp.Vector3(0, 0),
+    #                  size=mp.Vector3(50, 10, 0),
+    #                  amp_func=amp),
+    #        mp.Source(mp.CustomSource(src_func=time_dependence),
+    #                  component=mp.Ez,
+    #                  center=mp.Vector3(0, 0),
+    #                  size=mp.Vector3(50, 10, 0),
+    #                  amp_func=amp)]
+    # sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+    #                  component=mp.Ez,
+    #                  center=mp.Vector3(0, 0),
+    #                  size=mp.Vector3(50, 10, 0),
+    #                  amp_func=amp)]
     return sources
 
 
@@ -63,7 +102,8 @@ def amp(y):
     return amp*np.exp(1j * phase)
 
 def time_dependence(t):
-    return np.exp(1j*2*np.pi*0.15*t)
+    freq = 1/0.0167
+    return np.exp(1j*2*np.pi*freq*t)
 
 
 
@@ -128,15 +168,19 @@ if __name__=="__main__":
     #cell, geometry, sources, pml_layers = createDiffractionSlit()
     cell , geometry , sources , pml_layers = createSimulationSpace()
     resolution = globalVariables.resolution
+    sym = [mp.Mirror(mp.Y)]
     sim = mp.Simulation(cell_size=cell,
                     boundary_layers=pml_layers,
                     geometry=geometry,
+                    symmetries=sym,
                     sources=sources,
                     resolution=resolution)
     sim.use_output_directory()
     #sim.run(mp.at_beginning(mp.output_epsilon),
        # mp.to_appended("ez", mp.at_every(0.6, mp.output_png(mp.Ez,"-Zc dkbluered"))),
        # until=200)
-    sim.run(mp.at_every(0.6 , mp.output_png(mp.Ez, "-Zc dkbluered")), until=20)
+    sim.run(mp.at_every(1 , mp.output_png(mp.Ez, "-Zc dkbluered")), until_after_sources=70)
+    # sim.run(mp.at_every(0.6 , mp.output_png(mp.Ez, "-Zc dkbluered")), until=20)
+
     plot_data(sim,cell)
     inPlaceGifCreation()
