b7830da608fbcb8be95bad14f48e70cb07903cf1
dyunis <dyunis@uchicago.edu>
2019-02-11 16:51:29 -0600

    working version?

--- soltwisch2017.py
+++ soltwisch2017.py
@@ -1,107 +1,150 @@
+import math
+import cmath
 import meep as mp
 import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
-'''
-TODO: make sure resolutions, lengthscales, source frequencies make sense
-https://meep.readthedocs.io/en/latest/Introduction/#units-in-meep
-'''
-
-'''
-TODO: make reflectance plots (Fig 5, Fig 6)
-https://meep.readthedocs.io/en/latest/Python_Tutorials/Basics/#angular-reflectance-spectrum-of-a-planar-interface
-https://meep.readthedocs.io/en/latest/Python_Tutorials/Mode_Decomposition/#reflectance-and-transmittance-spectra-for-planewave-at-oblique-incidence
-'''
-
-#soltwisch use finite element to do the simulation, not FDTD
-#based on examples/binary_grating.py
+from midterm.materials import Silicon
 
 #set up geometry
-resolution = 10        # pixels/μm
-
-#G2 params in 10^-8m
+resolution = 10.
 dpml = 1.             # PML thickness
 dsub = 3.             # substrate thickness
 dpad = 3.             # air padding between grating and PML at top
-gp = 15.             # grating period
-gh = 11.950               # grating height
-lw = 6.730              #line width
-tr = 0.916              #top radius
-br = 1.302             #bottom radius
-sa = 84.73              #sidewall angle (deg)
+
+#G1 params in 10^-8m
+gp = 10.             # grating period
+gh = 10.271               # grating height
+lw = 5.404              #line width
+tr = 1.627              #top radius
+br = 1.579             #bottom radius
+sa = 90.91              #sidewall angle (deg)
+
+# #G2 params in 10^-8m
+# gp = 15.             # grating period
+# gh = 11.950               # grating height
+# lw = 6.730              #line width
+# tr = 0.916              #top radius
+# br = 1.302             #bottom radius
+# sa = 84.73              #sidewall angle (deg)
 
 a = gh/np.tan(sa*2*np.pi/360) # intermediate for vertex calculation
 
-sx = dpml+gp+dpml
+#2 periods of grating
+sx = dpml+2*gp+dpml
 sy = dpml+dsub+gh+dpad+dpml
 
-cell = mp.Vector3(sx,sy,0)
+cell_size = mp.Vector3(sx,sy,0)
 pml_layers = [mp.PML(thickness=dpml)]
-#pml_layers = [mp.PML(thickness=dpml,direction=mp.Y)]
 
-#check we have right epsilon
-substrate = mp.Medium(epsilon=12)
-grating = mp.Medium(epsilon=12)
+#set up material properties
+si = Silicon()
+ind = si.index_refract()
+ind = ind.real
+cond = ind.imag
+wv = 0.016869 #7.35 keV, relate to figure 6
+freq = 1/wv
+
+substrate = mp.Medium(index=ind, D_conductivity=2*math.pi*freq*cond/ind)
+grating = mp.Medium(index=ind, D_conductivity=2*math.pi*freq*cond/ind)
 
 #one for substrate, one for grating superstructure, using trapezoid prism to approximate right now
-vertices = [mp.Vector3(-0.5*lw-0.5*a,-0.5*gh,0),
-            mp.Vector3(0.5*lw+0.5*a,-0.5*gh,0),
-            mp.Vector3(0.5*lw-0.5*a,0.5*gh,0),
-            mp.Vector3(-0.5*lw+0.5*a,0.5*gh,0)]
+vtx = [mp.Vector3(-0.5*lw-0.5*a+gp/2,-0.5*gh,0),
+            mp.Vector3(0.5*lw+0.5*a+gp/2,-0.5*gh,0),
+            mp.Vector3(0.5*lw-0.5*a+gp/2,0.5*gh,0),
+            mp.Vector3(-0.5*lw+0.5*a+gp/2,0.5*gh,0)]
 
-#mp.Block(material=grating, size=mp.Vector3(lw,gh,mp.inf), center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0)),
+vtx2 = [mp.Vector3(-0.5*lw-0.5*a-gp/2,-0.5*gh,0),
+            mp.Vector3(0.5*lw+0.5*a-gp/2,-0.5*gh,0),
+            mp.Vector3(0.5*lw-0.5*a-gp/2,0.5*gh,0),
+            mp.Vector3(-0.5*lw+0.5*a-gp/2,0.5*gh,0)]
 
 geometry = [mp.Block(material=substrate, size=mp.Vector3(sx,dpml+dsub,mp.inf), center=mp.Vector3(0,-0.5*sy+0.5*(dpml+dsub),0)),
-            mp.Prism(vertices, height=mp.inf, center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0), material=grating)]
-# symmetries=[mp.Mirror(mp.X)]
-symmetries = []
+            mp.Prism(vtx, height=mp.inf, center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0), material=grating),
+            mp.Prism(vtx2, height=mp.inf, center=mp.Vector3(0,-0.5*sy+dpml+dsub+0.5*gh,0), material=grating)]
+sym = []
+# sym = [mp.Mirror(mp.Y)]
 
 #set up source
-wvl_min = 0.4           # min wavelength
-wvl_max = 0.6           # max wavelength
-fmin = 1/wvl_max        # min frequency
-fmax = 1/wvl_min        # max frequency
-fcen = 0.5*(fmin+fmax)  # center frequency
-df = fmax-fmin          # frequency width
+ng = 1 #index of medium wave is propagating in
 
-theta = 0.86 * 2*np.pi/360 #in fig 5a these are in interval [0.3,1.2]
-k = mp.Vector3(0,-np.sin(theta),np.cos(theta)) # complex fields for oblique angles
-src_pt = mp.Vector3(0,-0.5*sy+dpml+dsub+gh,0) # incident at top of grating
+theta = 1.09 * 2*np.pi/360 #in fig 6 these are in interval [0.3,1.2]
+k = mp.Vector3(0,-np.sin(theta),np.cos(theta)).scale(freq*ng) # complex fields for oblique angles
+src_pt = mp.Vector3(0,0,0) # plane of incidence in xy axis at front of cell
 
-#in meep, frequency units are in units of 2*pi*c
-#read more carefully how they set up simulation of source in paper, this is just a plane hitting the top of grating
-sources = [mp.Source(mp.ContinuousSource(frequency=100), component=mp.Ez, center=src_pt, size=mp.Vector3(sx,0,0))]
+def pw_amp(k,x0):
+    def _pw_amp(x):
+        return cmath.exp(1j*2*math.pi*k.dot(x+x0))
+    return _pw_amp
+
+sources = [mp.Source(mp.ContinuousSource(freq),
+                     component=mp.Ez,
+                     center=src_pt,
+                     size=mp.Vector3(sx-2*dpml,sy-2*dpml,0),
+                     amp_func=pw_amp(k,src_pt))]
 
 sim = mp.Simulation(resolution=resolution,
-                    cell_size=cell,
+                    cell_size=cell_size,
                     boundary_layers=pml_layers,
                     geometry=geometry,
                     k_point=k,
                     sources=sources,
-                    symmetries=symmetries)
-sim.run(until=20)
+                    symmetries=sym)
 
-eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
-ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+nearfield = sim.add_near2far(freq, 0, 1, mp.Near2FarRegion(mp.Vector3(0,0,0), size=mp.Vector3(sx-2*dpml,sy-2*dpml,0)))
 
-#plot geoms
-plt.figure(dpi=100)
-plt.imshow(eps_data.transpose()[::-1], interpolation='spline36', cmap='binary')
-plt.axis('off')
-plt.show()
+sim.run(until=100)
+
+Ez = np.zeros((101,2),dtype=np.complex128)
+for n in range(-50,51):
+    ffx = sim.get_farfield(nearfield, mp.Vector3(n,0,1e2))
+    ffy = sim.get_farfield(nearfield, mp.Vector3(0,n,1e2))
+    Ez[n,:] = (ffx[2],ffy[2])
+
+# sim.output_farfields(nearfield,
+#                     "ff-x-soltwisch2017",
+#                     resolution,
+#                     where=mp.Volume(mp.Vector3(0,0,1.5e4), size=mp.Vector3(100,0,0)))
+# sim.output_farfields(nearfield,
+#                     "ff-y-soltwisch2017",
+#                     resolution,
+#                     where=mp.Volume(mp.Vector3(0,0,1.5e4), size=mp.Vector3(0,100,0)))
+
+# d = np.zeros((101,101))
+# ez = np.abs(Ez)**2
+# for i in range(Ez.shape[0]):
+#     for j in range(Ez.shape[0]):
+#         d[i,j] = np.sqrt(ez[i,0]*ez[j,1])
+# plt.imshow(d, interpolation='spline36', cmap='RdBu')
+
+def I_q(x,E):
+    q = 2*np.pi/x
+    c = 1
+    n = 1
+    e0 = 1
+    A = c*n*e0/2
+    I = A*(np.abs(E)**2)
+
+    plt.plot(x*10,I)
+    plt.ylabel('Intensity')
+    plt.xlabel('d (nm)')
+    plt.show()
+    plt.plot(q/10,I)
+    plt.xlim(-0.7,0.7)
+    plt.ylabel('Intensity')
+    plt.xlabel('q (1/nm)')
+    plt.show()
+
+eps_data = sim.get_array(center=mp.Vector3(), size=cell_size, component=mp.Dielectric)
+ez_data = sim.get_array(center=mp.Vector3(), size=cell_size, component=mp.Ez)
 
 #plot geoms with field
-#is taking real part of field the right thing?
 plt.figure(dpi=100)
 plt.imshow(eps_data.transpose()[::-1], interpolation='spline36', cmap='binary')
-plt.imshow(ez_data.real.transpose()[::-1], interpolation='spline36', cmap='RdBu', alpha=0.9)
+plt.imshow((np.abs(ez_data)**2).transpose()[::-1], interpolation='spline36', cmap='RdBu', alpha=0.9)
 plt.axis('off')
 plt.show()
 
-plt.figure(dpi=100)
-plt.imshow(eps_data.transpose()[::-1], interpolation='spline36', cmap='binary')
-plt.imshow(ez_data.imag.transpose()[::-1], interpolation='spline36', cmap='RdBu', alpha=0.9)
-plt.axis('off')
-plt.show()
+I_q(np.arange(-50,51),Ez[:,0])
